5825701be9132c397484099d3371ebf487d94442
KeiretsuApps <andy.j.voelker@gmail.com>
2017-04-01 17:37:05 -0400

    Initial Commit

---
+++ In-App Purchase DemoTests/In_App_Purchase_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  In_App_Purchase_DemoTests.swift
+//  In-App Purchase DemoTests
+//
+//  Created by Andrew Voelker on 4/1/17.
+//  Copyright © 2017 Andrew Voelker. All rights reserved.
+//
+
+import XCTest
+@testable import In_App_Purchase_Demo
+
+class In_App_Purchase_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
